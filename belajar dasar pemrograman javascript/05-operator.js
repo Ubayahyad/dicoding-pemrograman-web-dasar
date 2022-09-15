494016e0f7b5cf42cee5449eadbf3c0d62874417
UbayAhyad <ahyadubay@gmail.com>
2022-09-15 08:33:54 +0700

    mengubah file tipe data.js

---
+++ belajar dasar pemrograman javascript/05-operator.js
@@ -0,0 +1,8 @@
+let x = 10;
+let y = 5;
+
+x += y; // artinya -> x = x + y;
+x -= y; // artinya -> x = x - y;
+x *= y; // artinya -> x = x * y;
+x /= y; // artinya -> x = x / y;
+x %= y; // artinya -> x = x % y;
