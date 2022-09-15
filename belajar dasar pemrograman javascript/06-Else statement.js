75445cf2c3112c232a506e4bc5bc5ab908f89a02
UbayAhyad <ahyadubay@gmail.com>
2022-09-15 09:27:28 +0700

    manambahkan file else statement

---
+++ belajar dasar pemrograman javascript/06-Else statement.js
@@ -0,0 +1,10 @@
+if (isRaining) {
+  console.log("Hari ini hujan. Bawa payung.");
+}
+console.log("Berangkat kegiatan.");
+
+/* output:
+Persiapan sebelum berangkat kegiatan.
+Hari ini hujan. Bawa payung.
+Berangkat kegiatan.
+ */
